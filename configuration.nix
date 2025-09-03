4c6ef54a9bdfd1ec7330eb016cab49d75b1d041a
kae3g <kj3x39@gmail.com>
2025-09-02 20:45:53 -0700

    Fix deprecated pinentryFlavor option
    - Changed pinentryFlavor to pinentryPackage = pkgs.pinentry-curses
    - Fixes nixos-rebuild switch error about deprecated option
    - Ready for successful system rebuild

--- configuration.nix
+++ configuration.nix
@@ -179,7 +179,7 @@
   services.pcscd.enable = true;
   programs.gnupg.agent = {
     enable = true;
-    pinentryFlavor = "curses";
+    pinentryPackage = pkgs.pinentry-curses;
     enableSSHSupport = true;
   };
 }
